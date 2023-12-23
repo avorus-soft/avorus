1ceb35051f677df6cbe042f00b0ce3c8c0d85823
Alexander Morosow <alexander.morosow@gmail.com>
2023-12-23 23:29:51 +0100

    upgrade install script

--- install/install.sh
+++ install/install.sh
@@ -6,30 +6,26 @@ inside_avorus_repo=false
 # Check if we're in the 'avorus' repo
 if [ -f "install/install.sh" ]; then
   inside_avorus_repo=true
-elif [ ! -d "avorus" ]; then
+fi
+
+if [ "$inside_avorus_repo" = false ]; then
   git clone -j8 --recurse-submodules https://github.com/avorus-soft/avorus
-  cd avorus || exit
-else
   cd avorus || exit
 fi
 
 # Define function to check and touch flag files
 check_and_touch() {
   if [ ! -f "$1" ]; then
-    "$2" && touch "$1"
+    $2 && touch "$1"
   else
-    echo "Skipping step, $1 flag exists."
+    echo "Skipping step, $1 exists."
   fi
 }
 
-# Only run environment setup if we're sure we're in the 'avorus' repo
-if [ "$inside_avorus_repo" = false ]; then
-  check_and_touch "env_setup.flag" "./install/env_setup.sh"
-  check_and_touch "ssl_setup.flag" "./install/ssl_setup.sh"
-  check_and_touch "docker_build.flag" "docker compose build"
-  check_and_touch "init_user.flag" "sudo docker compose run -it --rm api python3 init_user.py"
-fi
+check_and_touch "env_setup.flag" "./install/env_setup.sh"
+check_and_touch "ssl_setup.flag" "./install/ssl_setup.sh"
+check_and_touch "docker_build.flag" "docker compose build"
+check_and_touch "init_user.flag" "docker compose run -it --rm api python3 init_user.py"
 
 # Set up the frontend
 check_and_touch "frontend_setup.flag" "./install/frontend_setup.sh"
-
